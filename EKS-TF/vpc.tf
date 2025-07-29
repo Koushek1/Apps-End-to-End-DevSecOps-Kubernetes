7969a4569c3e1660001dbd5011d07e7078fe4043
Koushek1 <kousheksmenon8@gmail.com>
2025-07-29 14:34:05 +0530

    vpc.tf

--- EKS-TF/vpc.tf
+++ EKS-TF/vpc.tf
@@ -1,55 +1,88 @@
-data "aws_vpc" "vpc" {
-  filter {
-    name   = "tag:Name"
-    values = [var.vpc-name]
+resource "aws_vpc" "eks_vpc" {
+  cidr_block           = "10.11.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "eks-vpc"
   }
 }
 
-data "aws_internet_gateway" "igw" {
-  filter {
-    name   = "tag:Name"
-    values = [var.igw-name]
+resource "aws_internet_gateway" "eks_igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+
+  tags = {
+    Name = "eks-igw"
   }
 }
 
-data "aws_subnet" "subnet" {
-  filter {
-    name   = "tag:Name"
-    values = [var.subnet-name]
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.11.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "eks-public-subnet-1"
   }
 }
 
-data "aws_security_group" "sg-default" {
-  filter {
-    name   = "tag:Name"
-    values = [var.security-group-name]
-  }
-}
-
-resource "aws_subnet" "public-subnet2" {
-  vpc_id                  = data.aws_vpc.vpc.id
-  cidr_block              = "10.0.2.0/24"
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.11.2.0/24"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = var.subnet-name2
+    Name = "eks-public-subnet-2"
   }
 }
 
-resource "aws_route_table" "rt2" {
-  vpc_id = data.aws_vpc.vpc.id
+resource "aws_route_table" "eks_route_table" {
+  vpc_id = aws_vpc.eks_vpc.id
+
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = data.aws_internet_gateway.igw.id
+    gateway_id = aws_internet_gateway.eks_igw.id
   }
 
   tags = {
-    Name = var.rt-name2
+    Name = "eks-public-route-table"
   }
 }
 
-resource "aws_route_table_association" "rt-association2" {
-  route_table_id = aws_route_table.rt2.id
-  subnet_id      = aws_subnet.public-subnet2.id
+resource "aws_route_table_association" "public_1_association" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.eks_route_table.id
+}
+
+resource "aws_route_table_association" "public_2_association" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.eks_route_table.id
+}
+
+resource "aws_security_group" "eks_sg" {
+  name        = "eks-sg"
+  description = "Security group for EKS cluster"
+  vpc_id      = aws_vpc.eks_vpc.id
+
+  ingress {
+    description = "Allow all traffic within the VPC"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [aws_vpc.eks_vpc.cidr_block]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "eks-cluster-sg"
+  }
 }
